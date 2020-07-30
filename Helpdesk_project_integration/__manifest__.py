f97547b1029052ff3e4d907d15571e1ec979b5db
Fernando <fer.gvn18@gmail.com>
2020-07-30 16:02:59 +0200

    Added test integration for project module integration with helpdesk

---
+++ Helpdesk_project_integration/__manifest__.py
@@ -0,0 +1,16 @@
+
+{
+    'name': 'Helpdesk Project Integration',
+    'summary': """
+    Module for the integration of project in helpdesk module
+    """,
+    'version': '11.0.1.0.1',
+    'author': "Fernando Vasoncellos",
+    'license': "AGPL-3",
+    'maintainer': 'Fernando Vasconcellos',
+    'category': 'Helpdesk',
+    'website': 'https://odoo-community.org/',
+    'depends': [],
+    'data':[],
+    'installable': True,
+}
---
+++ Helpdesk_project_integration/tests/test_helpdesk_project_integration.py
@@ -0,0 +1,13 @@
+#Test of the ticket connected to a project
+
+"""
+
+def count_opened_ticket(self):
+    -> Check count before creating a new one 
+    -> Create new ticket inside a project, assigned default user 
+    -> Check if count is right (+1) and the count button is right
+
+def check_ticket_id_in_project(self):
+    -> Check if the new ticket created before is indeed in the right project 
+
+"""
